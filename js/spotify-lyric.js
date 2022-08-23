f552ec6d16cd2a310e9c66b9c9ea6d2a2e43c0c1
wzw-999 <63135037+wzw-999@users.noreply.github.com>
2022-08-23 12:02:50 +0800

    Update spotify-lyric.js

--- js/spotify-lyric.js
+++ js/spotify-lyric.js
@@ -30,7 +30,7 @@ http-response ^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/ s
 3.qx:
     - 自行配置MITM域名: spclient.wg.spotify.com
     - 手动修改填入下方的appid和securityKey密钥, 并配置重写,类型为script-response-body,
-      正则填入^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/
+      正则填入^https:\/\/spclient\.wg\.spotify\.com\/color-lyrics\/v2\/track\/https://raw.githubusercontent.com/wzw-999/rules/master/js/spotify-lyric.js
 */
 // 注意: QX用户需要手动填入appid和securityKey密钥, Surge和Loon用户无需填入!!!!
 const options = {
